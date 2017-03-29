140471afe65f6459c247033ee1f57719e40e0c77
Kevin Dyer <kevin.m.dyer@gmail.com>
2017-03-28 19:07:03 -0600

    Added cloud function to create user.

---
+++ functions/index.js
@@ -0,0 +1,49 @@
+(() => {
+  'use strict';
+
+  const crypto = require('crypto');
+  const uuid = require('uuid');
+  const functions = require('firebase-functions');
+  const admin = require('firebase-admin');
+
+  admin.initializeApp(functions.config().firebase);
+
+  const auth = admin.auth();
+  const database = admin.database();
+
+  function randomBytes(size) {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(size, (err, buf) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(buf);
+        }
+      });
+    });
+  }
+
+  exports.initialize = functions.https.onRequest((req, res) => {
+    const resInfo = {
+      email: null,
+      password: null
+    };
+    Promise.resolve()
+    .then(() => randomBytes(16))
+    .then((passwordBuffer) => {
+      resInfo.email = `${uuid.v4()}@my-device.com`;
+      resInfo.password = passwordBuffer.toString('hex');
+      return auth.createUser({
+        email: resInfo.email,
+        password: resInfo.password
+      });
+    })
+    .then(() => res.status(200).json(resInfo))
+    .catch((err) => res.status(400).json({
+      error: {
+        name: err.name,
+        message: err.message
+      }
+    }));
+  });
+})();
